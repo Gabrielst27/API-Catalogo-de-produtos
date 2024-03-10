dd0622a4097805940c14dfde67ba30a138c72638
Gabriel Silva Torres <gabrielst328@gmail.com>
2024-03-09 21:37:40 -0300

    Models Produto e Categoria

---
+++ APICatalogo/APICatalogo/Models/Categoria.cs
@@ -0,0 +1,10 @@
+﻿namespace APICatalogo.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        public string? Nome { get; set; }
+        public string? ImgUrl { get; set; }
+
+    }
+}
---
+++ APICatalogo/APICatalogo/Models/Produto.cs
@@ -0,0 +1,16 @@
+﻿namespace APICatalogo.Models
+{
+    public class Produto
+    {
+
+        public int ProdutoId { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public string ImgUrl { get; set; }
+        public float Estoque { get; set; }
+        public DateTime DataCadastro { get; set; }
+
+
+    }
+}
