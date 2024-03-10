5049581637c0a128a3efe78daaf766fdc0416b43
Gabriel Silva Torres <gabrielst328@gmail.com>
2024-03-10 00:14:34 -0300

    DbContext

--- APICatalogo/APICatalogo/Models/Produto.cs
+++ APICatalogo/APICatalogo/Models/Produto.cs
@@ -4,10 +4,10 @@
     {
 
         public int ProdutoId { get; set; }
-        public string Nome { get; set; }
-        public string Descricao { get; set; }
-        public decimal Preco { get; set; }
-        public string ImgUrl { get; set; }
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }
+        public decimal? Preco { get; set; }
+        public string? ImgUrl { get; set; }
         public float Estoque { get; set; }
         public DateTime DataCadastro { get; set; }
 
--- APICatalogo/APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4edfe68a11bb229cdd057ddd3a179b98e01f7b48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd0622a4097805940c14dfde67ba30a138c72638")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
