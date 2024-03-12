0f881427bf220e2e37ef787df5eb52caf1550b37
Gabriel Silva Torres <gabrielst328@gmail.com>
2024-03-11 22:53:31 -0300

    Relacionamento Produtos e Categorias, Migrations e Data Annotations

--- APICatalogo/APICatalogo/Models/Categoria.cs
+++ APICatalogo/APICatalogo/Models/Categoria.cs
@@ -1,10 +1,30 @@
-﻿namespace APICatalogo.Models
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Cryptography.X509Certificates;
+
+namespace APICatalogo.Models
 {
+    [Table("Categorias")]
     public class Categoria
     {
+        public Categoria()
+        {
+            Produtos = new Collection<Produto>();
+        }
+
+        [Key]
         public int CategoriaId { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string? Nome { get; set; }
+
+        [Required]
+        [StringLength(500)]
         public string? ImgUrl { get; set; }
+
+        public ICollection<Produto>? Produtos { get; set; }
 
     }
 }
--- APICatalogo/APICatalogo/Models/Produto.cs
+++ APICatalogo/APICatalogo/Models/Produto.cs
@@ -1,16 +1,35 @@
-﻿namespace APICatalogo.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APICatalogo.Models
 {
+    [Table("Produtos")]
     public class Produto
     {
-
+        [Key]
         public int ProdutoId { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string? Nome { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string? Descricao { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(15,2)")]
         public decimal? Preco { get; set; }
+
+        [Required]
+        [StringLength(500)]
         public string? ImgUrl { get; set; }
+
         public float Estoque { get; set; }
         public DateTime DataCadastro { get; set; }
 
+        public int CategoriaId { get; set; }
+        public Categoria? Categoria { get; set; }
 
     }
 }
