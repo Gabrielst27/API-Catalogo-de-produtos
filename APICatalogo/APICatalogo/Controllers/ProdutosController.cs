ceefae21b63c90a4615bd37d4268068a1ba6ad97
Gabriel Silva Torres <gabrielst328@gmail.com>
2024-03-18 22:35:56 -0300

    Método Post

--- APICatalogo/APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/APICatalogo/Controllers/ProdutosController.cs
@@ -16,14 +16,14 @@ namespace APICatalogo.Controllers
         }
 
         //Buscar objeto da classe Produto pelo id
-        [HttpGet("{id}")]
+        [HttpGet("{id:int}", Name = "ObterProduto")]
         public ActionResult<Produto> GetById(int id)
         {
             var produto = _context.Produtos.Find(id);
 
-            if (produto == null)
+            if (produto is null)
             {
-                return NotFound();
+                return NotFound("Produto não encontrado");
             }
 
             return produto;
@@ -34,6 +34,21 @@ namespace APICatalogo.Controllers
         public ActionResult<IEnumerable<Produto>> GetAll()
         {
             return _context.Produtos.ToList();
+        }
+
+        //Inserir novo objeto da classe Produto
+        [HttpPost]
+        public ActionResult Post(Produto produto)
+        {
+            if (produto is null)
+            {
+                return BadRequest();
+            }
+
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult ("ObterProduto", new {id = produto.ProdutoId}, produto);
         }
     }
 }
--- APICatalogo/APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b85242745b42158b4cb1e197c01ca585ed8d37f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60a68aea1160bf7852b572a59bc0bc5cfd26ea99")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
