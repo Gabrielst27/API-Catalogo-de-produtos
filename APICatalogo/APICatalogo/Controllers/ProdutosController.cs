c3de4b6847bd334f28ed25dcf7170b0dcbb3eb1c
Gabriel Silva Torres <gabrielst328@gmail.com>
2024-03-19 22:30:18 -0300

    Métodos Put e Delete

--- APICatalogo/APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/APICatalogo/Controllers/ProdutosController.cs
@@ -1,6 +1,7 @@
 ﻿using APICatalogo.Context;
 using APICatalogo.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace APICatalogo.Controllers
 {
@@ -15,7 +16,7 @@ namespace APICatalogo.Controllers
             _context = context;
         }
 
-        //Buscar objeto da classe Produto pelo id
+        //Buscar objeto da classe Produto por id
         [HttpGet("{id:int}", Name = "ObterProduto")]
         public ActionResult<Produto> GetById(int id)
         {
@@ -48,7 +49,39 @@ namespace APICatalogo.Controllers
             _context.Produtos.Add(produto);
             _context.SaveChanges();
 
-            return new CreatedAtRouteResult ("ObterProduto", new {id = produto.ProdutoId}, produto);
+            return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId }, produto);
+        }
+
+        //Atualizar Produto por id
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Produto produto)
+        {
+            if (id != produto.ProdutoId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(produto).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(produto);
+        }
+
+        //Deletar Produto por id
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var produto = _context.Produtos.Find(id);
+
+            if (produto is null)
+            {
+                return NotFound();
+            }
+
+            _context.Remove(produto);
+            _context.SaveChanges();
+
+            return Ok(produto);
         }
     }
 }
--- APICatalogo/APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60a68aea1160bf7852b572a59bc0bc5cfd26ea99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ceefae21b63c90a4615bd37d4268068a1ba6ad97")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
