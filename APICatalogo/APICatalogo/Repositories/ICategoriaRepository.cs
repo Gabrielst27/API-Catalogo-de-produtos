fc59dd14f83542d1170c0f7e19d97a8cb4581ea2
Gabriel Silva Torres <gabrielst328@gmail.com>
2024-05-13 23:12:41 -0300

    implementando o padrão repository (repositorio e interface de repositorio da categoria)

---
+++ APICatalogo/APICatalogo/Repositories/ICategoriaRepository.cs
@@ -0,0 +1,14 @@
+﻿using APICatalogo.Models;
+using System.Runtime.InteropServices;
+
+namespace APICatalogo.Repositories
+{
+    public interface ICategoriaRepository
+    {
+        IEnumerable<Categoria> GetCategorias();
+        Categoria GetCategoria(int id);
+        Categoria Create(Categoria categoria);
+        Categoria Update(Categoria categoria);
+        Categoria Delete(int id);
+    }
+}
--- APICatalogo/APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
+++ APICatalogo/APICatalogo/obj/Debug/net8.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+932e85e2b526b76f8de4d4ec8146562e775b3240")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71f7c955cf44d825ca36d732c7d6546c0c327a66")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
